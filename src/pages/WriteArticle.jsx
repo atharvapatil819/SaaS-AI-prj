945a12ffe5b24872d6fcfe9f9e5c31d087f54cf4
atharvapatil819 <patilatharva0708@gmail.com>
2026-08-30 15:39:06 +0530

    articles

--- src/pages/WriteArticle.jsx
+++ src/pages/WriteArticle.jsx
@@ -1,8 +1,39 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { Sparkles } from 'lucide-react'
 
-const WriteArticle = () => {
+const WriteArticle = () => {  
+
+  const ArticleLenght= [
+    {length:800, Text: 'short(500-800 words'},
+    {length:1200, Text: 'medium(800-1200 words'},
+    {length:1600, Text: 'long(1200+ words'},
+  ] 
+
+  const [selectedLenght,setSelectedLenght] = useState(ArticleLenght[0]);
+  const[input,setInput]= useState('');
+
+
   return (
-    <div>WriteArticle</div>
+    <div className='h-full overflow-y-scroll p-6 flex items-start flex-wrap gap-4 text-slate-700'>
+
+      <form className='w-full max-w-lg p-4 bg-white rounded-lg border border-gray-200'>
+        <div className='flex items-center gap-3'>
+          <Sparkles className=" w-6 text-[#4A7AFF]" />
+          <h1 className='text-xl font-semibold'>Article Configuration</h1>
+        </div> 
+        <p className='mt-6 text-sm font-medium'>Article Topic</p> 
+        <input type='text' className='w-full p-2 px-3 mt-2 outline-none text-sm rounded-md border border-gray-300' placeholder="The future of artificial intelligence is..." required /> 
+        <p class="mt-4 text-sm font-medium">Article Length</p> 
+        <div className='mt-3 flex gap-3 flex-wrap sm:max-w-9/11'>
+          {ArticleLenght.map((item,index)=>(
+            <span onClick={()=> setSelectedLenght(item)} className={`text-xs px-4 py-1  border rounded-full cursor-pointer ${selectedLenght.Text===item.Text }? 'bg-blue-50 text-blue-700' :'text-gray-500 border-gray-300'}`} key={index}>
+              {item.Text}
+            </span>
+          ))}
+        </div>
+      </form>
+
+    </div>
   )
 }
 
