e3210cce68ca0495962f122e85210d1bc4156634
atharvapatil819 <patilatharva0708@gmail.com>
2026-08-31 23:03:42 +0530

    writearticles

--- src/pages/BlogTitles.jsx
+++ src/pages/BlogTitles.jsx
@@ -1,8 +1,55 @@
 import React from 'react'
+import { Edit, Sparkles, Target } from 'lucide-react'
 
-const BlogTitles = () => {
+const BlogTitles = () => { 
+  const blogcategories= [] 
+  
+    const [selectedLenght,setSelectedLenght] = useState(ArticleLenght[0]);
+    const[input,setInput]= useState('');
+    const onSubmitHandler = async (e) =>{
+      e.preventDefault();
+    }
   return (
-    <div>BlogTitles</div>
+    <div className='h-full overflow-y-scroll p-6 flex items-start flex-wrap gap-4 text-slate-700'>
+
+      <form onSubmit={onSubmitHandler} className='w-full max-w-lg p-4 bg-white rounded-lg border border-gray-200'>
+        <div className='flex items-center gap-3'>
+          <Sparkles className=" w-6 text-[#4A7AFF]" />
+          <h1 className='text-xl font-semibold'>Article Configuration</h1>
+        </div> 
+        <p className='mt-6 text-sm font-medium'>Article Topic</p> 
+        <input onChange={(e) => setInput(e.target.value)} value={input}  className='w-full p-2 px-3 mt-2 outline-none text-sm rounded-md border border-gray-300' placeholder="The future of artificial intelligence is..." required /> 
+        <p class="mt-4 text-sm font-medium">Article Length</p> 
+        <div className='mt-3 flex gap-3 flex-wrap sm:max-w-9/11'>
+          {ArticleLenght.map((item,index)=>(
+            <span onClick={()=> setSelectedLenght(item)} className={`text-xs px-4 py-1  border rounded-full cursor-pointer ${selectedLenght.Text===item.Text ? 'bg-blue-50 text-blue-700' :'text-gray-500 border-gray-300'}`} key={index}>
+              {item.Text}
+            </span> 
+          ))}
+          <div>
+            <br />
+            <button className='w-full flex justify-center items-center gap-2 bg-gradient-to-r from-[#226BFF] to-[#65ADFF] text-white px-4 py-2 mt-6 text-sm rounded-lg cursor-pointer'>
+              <Edit className='w-5' /> 
+              Genrate Article
+            </button>
+          </div>
+        </div>
+      </form>
+      <div className='w-full max-w-lg p-4 bg-white rounded-lg flex flex-col border border-gray-200 min-h-96 max-h-150'>
+        <div className='flex items-center gap-3'>
+          <Edit className='w-5 h-5 text-[#4A7AFF]'/>
+          <h1 className='text-xl font-semibold'>Article Configuration</h1>
+        </div> 
+        <div className='flex-1 flex justify-center items-center'>
+          <div className='text-sm flex flex-col items-center gap-5 text-gray-400'>
+            <Edit className='w-9 h-9 text-[#4A7AFF]' />
+            <p>Enter a topic and click “Generate article ” to get started</p>
+          </div>
+        </div>
+
+      </div>
+
+    </div>
   )
 }
 
