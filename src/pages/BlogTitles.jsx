beb513faadfa01591788ead9a0332473ad8f0c74
atharvapatil819 <patilatharva0708@gmail.com>
2026-09-03 15:59:15 +0530

    frotn end

--- src/pages/BlogTitles.jsx
+++ src/pages/BlogTitles.jsx
@@ -1,10 +1,10 @@
-import React from 'react'
-import { Edit, Sparkles, Target } from 'lucide-react'
+import React, { useState } from 'react'
+import { Edit, Hash, Sparkles, Target } from 'lucide-react'
 
 const BlogTitles = () => { 
-  const blogcategories= [] 
+  const blogcategories= ['Genral','Technology','Buissness','Health','Lifestyle','Education','Travel','Food'] 
   
-    const [selectedLenght,setSelectedLenght] = useState(ArticleLenght[0]);
+    const [selectedCategory,setSelectedCategory] = useState('Genral');
     const[input,setInput]= useState('');
     const onSubmitHandler = async (e) =>{
       e.preventDefault();
@@ -14,36 +14,36 @@ const BlogTitles = () => {
 
       <form onSubmit={onSubmitHandler} className='w-full max-w-lg p-4 bg-white rounded-lg border border-gray-200'>
         <div className='flex items-center gap-3'>
-          <Sparkles className=" w-6 text-[#4A7AFF]" />
-          <h1 className='text-xl font-semibold'>Article Configuration</h1>
+          <Sparkles className=" w-6 text-[#8E37EB]" />
+          <h1 className='text-xl font-semibold'>AI Title Genrator</h1>
         </div> 
-        <p className='mt-6 text-sm font-medium'>Article Topic</p> 
+        <p className='mt-6 text-sm font-medium'>Keyword</p> 
         <input onChange={(e) => setInput(e.target.value)} value={input}  className='w-full p-2 px-3 mt-2 outline-none text-sm rounded-md border border-gray-300' placeholder="The future of artificial intelligence is..." required /> 
-        <p class="mt-4 text-sm font-medium">Article Length</p> 
+        <p class="mt-4 text-sm font-medium">Category</p> 
         <div className='mt-3 flex gap-3 flex-wrap sm:max-w-9/11'>
-          {ArticleLenght.map((item,index)=>(
-            <span onClick={()=> setSelectedLenght(item)} className={`text-xs px-4 py-1  border rounded-full cursor-pointer ${selectedLenght.Text===item.Text ? 'bg-blue-50 text-blue-700' :'text-gray-500 border-gray-300'}`} key={index}>
-              {item.Text}
+          {blogcategories.map((item,index)=>(
+            <span onClick={()=> setSelectedCategory(item)} className={`text-xs px-4 py-1  border rounded-full cursor-pointer ${selectedCategory===item ? 'bg-purple-50 text-purple-700' :'text-gray-500 border-gray-300'}`} key={item}>
+              {item}
             </span> 
           ))}
           <div>
             <br />
             <button className='w-full flex justify-center items-center gap-2 bg-gradient-to-r from-[#226BFF] to-[#65ADFF] text-white px-4 py-2 mt-6 text-sm rounded-lg cursor-pointer'>
-              <Edit className='w-5' /> 
-              Genrate Article
+              <Hash className='w-5' /> 
+              Genrate Title
             </button>
           </div>
         </div>
       </form>
-      <div className='w-full max-w-lg p-4 bg-white rounded-lg flex flex-col border border-gray-200 min-h-96 max-h-150'>
+      <div className='w-full max-w-lg p-4 bg-white rounded-lg flex flex-col border border-gray-200 min-h-96'>
         <div className='flex items-center gap-3'>
-          <Edit className='w-5 h-5 text-[#4A7AFF]'/>
-          <h1 className='text-xl font-semibold'>Article Configuration</h1>
+          <Hash className='w-5 h-5 text-[#8E37EB]'/>
+          <h1 className='text-xl font-semibold'>Genrated Titles</h1>
         </div> 
         <div className='flex-1 flex justify-center items-center'>
           <div className='text-sm flex flex-col items-center gap-5 text-gray-400'>
-            <Edit className='w-9 h-9 text-[#4A7AFF]' />
-            <p>Enter a topic and click “Generate article ” to get started</p>
+            <Hash className='w-9 h-9 text-[#4A7AFF]' />
+            <p>Enter a topic and click “Generate Title ” to get started</p>
           </div>
         </div>
 
